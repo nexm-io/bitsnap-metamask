0b73e43e4f33968785f97be7020a56c57454ee63
Trong Dinh <trongdth@gmail.com>
2024-01-02 13:55:02 +0700

    [wip] configure ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  preset: '@metamask/snaps-jest'
+  preset: '@metamask/snaps-jest',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__test__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  verbose: true,
 };
